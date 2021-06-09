943ed99d553df2761d5807afbaa6b714022b2f43
ZoZoHong <1161113466@qq.com>
2021-06-09 21:35:47 +0800

    6.9 dp

---
+++ 每日一题/dp/profitableSchemes879.js
@@ -0,0 +1,33 @@
+/**
+ * dp hard  背包问题
+ * https://leetcode-cn.com/problems/profitable-schemes/
+ * @param {number} n
+ * @param {number} minProfit
+ * @param {number[]} group
+ * @param {number[]} profit
+ * @return {number}
+ */
+var profitableSchemes = function (n, minProfit, group, profit) {
+    const len = group.length, MOD = 1e9 + 7;
+    const dp = new Array(len + 1).fill(0).map(() => new Array(n + 1).fill(0).map(() => new Array(minProfit + 1).fill(0)));
+    dp[0][0][0] = 1;
+    for (let i = 1; i <= len; i++) {
+        const mebers = group[i - 1], earn = profit[i - 1];
+        for (let j = 0; j <= n; j++) {
+            for (let k = 0; k <= minProfit; k++) {
+                if (j < mebers) {
+                    dp[i][j][k] = dp[i - 1][j][k];
+                } else {
+                    dp[i][j][k] = (dp[i - 1][j][k] + dp[i - 1][j - mebers][Math.max(0, k - earn)]) % MOD;
+                }
+
+            }
+        }
+
+    }
+    let sum = 0;
+    for (let j = 0; j <= n; j++) {
+        sum = (sum + dp[len][j][minProfit]) % MOD;
+    }
+    return sum;
+};
