0e0949e3ae430805555323b4044d4947b99c39a7
ZoZoHong <1161113466@qq.com>
2021-06-21 10:21:25 +0800

    二进制手表

---
+++ 每日一题/readBinaryWatch401.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} turnedOn
+ * @return {string[]}
+ */
+var readBinaryWatch = function (turnedOn) {
+    // 枚举时分
+    let ans = [];
+
+    for (let i = 0; i < 12; i++) {
+        for (let j = 0; j < 60; j++) {
+            if (i.toString(2).split('0').join('').length + j.toString(2).split('0').join('').length === turnedOn) {
+                ans.push(i + ':' + j < 10 ? 0 + j : j);
+            }
+        }
+    }
+    return ans;
+};
