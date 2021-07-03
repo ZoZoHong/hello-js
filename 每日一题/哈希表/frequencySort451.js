24728925ddcdf3067b02d79d3452eaf7010833c6
ZoZoHong <1161113466@qq.com>
2021-07-03 10:30:20 +0800

    7.3 频率排序

---
+++ 每日一题/哈希表/frequencySort451.js
@@ -0,0 +1,58 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+// var frequencySort = function (s) {
+//     let ans = [];
+//     let res = [];
+//     s = s.split('');
+//     //哈希
+//     const map = new Map();
+//     for (let c of s) {
+//         map.set(c, (map.get(c) || 0) + 1);
+//     }
+//     // 遍历排序
+//     for (let [k, v] of map.entries()) {
+//         ans.push(k);
+//     }
+//     ans.sort((a, b) => map.get(b) - map.get(a));
+//     for (let c of ans) {
+//         for (let i = 0; i < map.get(c); i++) {
+//             res.push(c);
+//         }
+//     }
+//     return res.join('');
+// };
+
+// let s = 'cccaaa';
+// console.log(frequencySort(s));
+
+
+// 参考昨天的计数排序 => 桶排序
+var frequencySort = function (s) {
+    const map = new Map();
+    let maxfreq = 0;
+
+    for (const ch of s) {
+        const frequency = (map.get(ch) || 0) + 1;
+        map.set(ch, frequency);
+        maxfreq = Math.max(maxfreq, frequency);
+    }
+    const buckets = new Array(maxfreq + 1).fill(0).map(() => new Array());
+    for (const [ch, num] of map.entries()) {
+        buckets[num].push(ch);
+    }
+    const ans = [];
+    for (let i = maxfreq; i > 0; i--) {
+        const bucket = buckets[i];
+        for (const ch of bucket) {
+            for (k = 0; k < i; k++) {
+                ans.push(ch);
+            }
+        }
+    }
+    return ans.join('');
+}
+
+let s = 'cccaaa';
+console.log(frequencySort(s));
--- 每日一题/哈希表/tempCodeRunnerFile.js
+++ 每日一题/哈希表/tempCodeRunnerFile.js
@@ -1,2 +1,24 @@
+var frequencySort = function (s) {
+//     let ans = [];
+//     let res = [];
+//     s = s.split('');
+//     //哈希
+//     const map = new Map();
+//     for (let c of s) {
+//         map.set(c, (map.get(c) || 0) + 1);
+//     }
+//     // 遍历排序
+//     for (let [k, v] of map.entries()) {
+//         ans.push(k);
+//     }
+//     ans.sort((a, b) => map.get(b) - map.get(a));
+//     for (let c of ans) {
+//         for (let i = 0; i < map.get(c); i++) {
+//             res.push(c);
+//         }
+//     }
+//     return res.join('');
+// };
 
-        }
+// let s = 'cccaaa';
+// console.log(frequencySort(s));
